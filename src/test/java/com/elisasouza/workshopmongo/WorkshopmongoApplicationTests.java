a8c31ec8625c9273dc9271a9859eb103ba1878b3
Elisa Souza <contato.elisasouza@hotmail.com>
2025-08-22 12:58:34 -0300

    First commit

---
+++ src/test/java/com/elisasouza/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elisasouza.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
